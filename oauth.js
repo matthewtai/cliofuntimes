a448e4a526c709e7c042028495fc69710230313e
Matthew Tai <matthewtai@gmail.com>
2022-08-16 01:50:13 -0400

    css

---
+++ oauth.js
@@ -0,0 +1,40 @@
+//OAuth 
+
+function saveKeyandSecret() {
+    console.log("Key and Secret Saved")
+    var formKey = document.getElementById('clientkey').value;
+    var formSecret = document.getElementById('clientsecret').value;
+    sessionStorage.setItem("clientkey", formKey);
+    sessionStorage.setItem("clientsecret", formSecret);
+}
+
+function authorize() {
+    var key = sessionStorage.getItem("clientkey");
+    var redirect_uri = 'https://staging.clio.com/oauth/approval';
+    var authorizeurl = 'https://staging.clio.com/oauth/authorize?response_type=code&client_id=' + key + '&redirect_uri=' +
+    redirect_uri;
+    console.log("I'M AUTHORIZING!");
+    window.open(authorizeurl);
+}
+//Save Access Code
+function saveAccessToken() {
+    var key = sessionStorage.getItem("clientkey");
+    var secret = sessionStorage.getItem("clientsecret");
+    var redirect_uri = 'https://staging.clio.com/oauth/approval';
+    var authorizationCode = document.getElementById('authcode').value;
+    sessionStorage.setItem("authorizationcode", authorizationCode);
+    var tokenurl =
+        'https://evening-fjord-72698.herokuapp.com/https://staging.clio.com/oauth/token?client_id=' + key + '&client_secret=' + secret + '&grant_type=authorization_code&code=' + authorizationCode + '&redirect_uri=' + redirect_uri
+    fetch(tokenurl, {
+        method: 'post',
+        headers: {
+            'Host': 'app.clio.com',
+            'Connection': "keep-alive",
+            'Content-Type': "application/x-www-form-urlencoded"
+        }
+
+    }).then((response) => response.json())
+        .then((data) => sessionStorage.setItem("accesstoken", data.access_token));
+
+
+}
